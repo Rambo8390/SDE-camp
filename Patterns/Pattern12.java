20918eb84b3f1a54bbcdc0016f078e2c9ed8a9b3
Sagnik Purkait <72170247+Rambo8390@users.noreply.github.com>
2022-02-04 05:45:14 +0530

    Update Pattern12.java

--- Patterns/Pattern12.java
+++ Patterns/Pattern12.java
@@ -12,11 +12,20 @@ public class Pattern12 {
 		
 		for(int i = 0 ; i < n ; i++)
 		{
+			int k = i+1;
 			for(int j = 0 ; j <= 2*(n-1) ; j++)
 			{
 				if(j >= (n-1-i) && j-i <= (n-1) )
 				{
-					System.out.print("*");
+					if(j < n-1)
+					{
+						System.out.print(k);
+						k--;
+					}
+					else {
+						System.out.print(k);
+						k++;
+					}
 				}
 				else {
 					System.out.print(" ");
@@ -27,11 +36,20 @@ public class Pattern12 {
 		
 		for(int i = n-2 ; i >= 0 ; i--)
 		{
+			int k = i+1;
 			for(int j = 0 ; j <= 2*(n-1) ; j++)
 			{
 				if(j >= (n-1-i) && j-i <= (n-1) )
 				{
-					System.out.print("*");
+					if(j < n-1)
+					{
+						System.out.print(k);
+						k--;
+					}
+					else {
+						System.out.print(k);
+						k++;
+					}
 				}
 				else {
 					System.out.print(" ");
